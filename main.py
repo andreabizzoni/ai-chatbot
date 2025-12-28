b3edfe64489cb42adfe9bc16cf34bd7a4428bd77
andreabizzoni <andrea.bizzoni@outlook.com>
2025-12-28 20:47:58 +0100

    brainstorming

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+from docling.document_converter import DocumentConverter
+
+
 def main():
-    print("Hello from ai-chatbot!")
+    converter = DocumentConverter()
+    result = converter.convert("data/fertil_catalog.pdf")
+    document = result.document
+    text = document.export_to_markdown()
+    print(text)
 
 
 if __name__ == "__main__":
